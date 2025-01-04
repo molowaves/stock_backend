fc63e4d3a1c780685b37fa518bbd52404ff0e0fd
molowaves <molosounds@gmail.com>
2025-01-04 12:08:11 +0100

    Added user model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=20)
+    is_email_verified = models.BooleanField(default=False)
+    is_phone_verified = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.username
